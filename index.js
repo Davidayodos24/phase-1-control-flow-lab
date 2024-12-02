3b59c2070d3c3161948e7fa31955c0ca9be3dd14
Ayodo <davidayodos24@gmail.com>
2024-12-02 19:34:33 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 400 && distance <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// Example test structure
+describe('index.js', function () {
+  describe('scuberGreetingForFeet()', function () {
+    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
+    });
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
+      expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
+    });
+
+    it('charges 30 dollars for a distance over 2000 feet', function () {
+      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+    });
+
+    it('does not allow rides over 2500 feet', function () {
+      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+    });
+  });
+});
+function ternaryCheckCity(city) { 
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'; 
+}
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
